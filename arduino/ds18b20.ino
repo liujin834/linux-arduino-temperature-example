f9f83bf8abe2a208dcdcca2920a0b80194a306ce
Jianxuan Li <la5c@qq.com>
2015-04-22 23:46:35 +0800

    add ds18b20 sensor source code

---
+++ arduino/ds18b20.ino
@@ -0,0 +1,24 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//the pin you connect the ds18b20 to
+#define DS18B20 2
+
+OneWire ourWire(DS18B20);
+DallasTemperature sensors(&ourWire);
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+  //start reading
+  sensors.begin();
+}
+
+void loop()
+{
+  //read temperature and output via serial
+  sensors.requestTemperatures();
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println(" degrees C");
+}
