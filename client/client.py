29339e1228a5a0c6c7964ae013028c800d3dee9d
Jianxuan Li <la5c@qq.com>
2015-05-09 23:09:52 +0800

    add curl

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import pycurl
+import cStringIO
 
 ser = serial.Serial('/dev/cu.usbmodem1431', 9600, timeout=1)
 while True:
@@ -11,6 +13,24 @@ while True:
 
     if len(string) == 5:
         print str(string)
+        c = pycurl.Curl()
+        host = 'http://localhost:8009/?'
+        url = [
+            'client=db18fda-cdf-401f-a560-0cfdfaead4e',
+            'ts_observe='+time.strftime("%Y-%m-%d")+'%20'+time.strftime("%H:%M:%S"),
+            'val='+str(string)
+        ]
+
+        uri = host + '&'.join(url)
+
+        c.setopt(c.URL, uri)
+        c.perform()
+
+        buf = cStringIO.StringIO()
+        print buf.getvalue()
+        buf.close()
+
+        time.sleep(600)
     else:
         print "abort"
-    time.sleep(3)
+        time.sleep(1)
